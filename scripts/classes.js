9eebbf2bcf8577954e9b295bdacecc1dba89895b
neo <chrissiguetter2001@gmail.com>
2024-06-10 22:39:03 +0200

    fix bug in decompression, add table, add extra buttons, light styling

--- scripts/classes.js
+++ scripts/classes.js
@@ -1,3 +1,10 @@
+/**
+ * Inserts a given value into a given array at the given index
+ * @param array {*[]} Array where value is inserted into.
+ * @param index {number} Index at which new value is inserted.
+ * @param value {Any} New value to be inserted into the array.
+ * @returns {*[]} New array with the inserted value at the given index.
+ */
 function insertAtIndex(array, index, value) {
     return [
         ...array.slice(0, index),
@@ -9,23 +16,58 @@ function insertAtIndex(array, index, value) {
 export class LwzConverter {
     constructor() {
         this.#dictionary = new Map();
+        this.#stepCollection = [];
     }
 
     #dictionary = new Map();
+    #stepCollection = [];
 
-    emptyDictionary() {
+    /**
+     * Utility function to empty the dictionary and step collection after/before use.
+     */
+    emptyArrays() {
         this.#dictionary.clear();
+        this.#stepCollection.length = 0;
     }
 
+    buildStep(string, z, newEntry) {
+        let entry = "";
+
+        if (newEntry === undefined) entry = "-";
+        else entry = `${newEntry[1]} => ${newEntry[0]}`;
+
+        return {S: string, z: z, newEntry: entry};
+    }
+
+    /**
+     * Utility function for working with single strings and compound entries in the dictionary.
+     * @param {String} string A string which can be a single character or a composed character, e.g. <265>A or whitespace
+     * @returns {*|string} String itself, a character representing the whitespace or the respective address for that string.
+     */
     getStringFromDictionary(string) {
-        if (string === "" || string.length === 1) {
-            return string;
-        }
-        else {
-            return `<${this.#dictionary.get(string)}>`;
+        switch (true) {
+            case string === " ":
+                return "_";
+
+            case string === "\n":
+                return "/";
+
+            case string === "":
+                return string;
+
+            case string.length === 1:
+                return string;
+
+            default:
+                return `<${this.#dictionary.get(string)}>`;
         }
     }
 
+    /**
+     * Utility function to access a string from the dictionary, as the addresses are the values in the dictionary.
+     * @param {String} address Address to be accessed in the format 265 (three-digit number)
+     * @returns {string} String associated with the address, or an error.
+     */
     getStringByAddress(address) {
         let string = "";
         for (let entry of this.#dictionary.entries()) {
@@ -34,11 +76,14 @@ export class LwzConverter {
                 break;
             }
         }
-        console.log(`entered address: ${address}, returned string: ${string}`)
         if (string !== "") return string;
         else throw `Address '${address}' not found in dictionary.`;
     }
 
+    /**
+     * Utility function to turn the dictionary into a string to print or add to text areas.
+     * @returns {string} The dictionary as a string in the form of address: string
+     */
     stringifyDictionary() {
         let string = "";
         for (const entry of this.#dictionary.entries()) {
@@ -48,6 +93,11 @@ export class LwzConverter {
         return string;
     }
 
+    /**
+     * Turns a compressed text into an array, splitting it into single letters and addresses
+     * @param compressedText A LZW compressed text containing letters and addresses
+     * @returns {*[]} Text split up into an array by letter and address, e.g. ['A', 'B', '<256>']
+     */
     splitCompressedText(compressedText) {
         let splitText = [];
         let i = 0;
@@ -65,62 +115,94 @@ export class LwzConverter {
         return splitText;
     }
 
+    /**
+     * Compresses a string using the LZW algorithm.
+     * @param {String} decompressedText The text to be compressed.
+     * @returns {{dictionary: Map<any, any>, compressedText: string, steps: []}} An object containing the resulting dictionary from the compression and the compressed text.
+     */
     compressText(decompressedText) {
-        this.emptyDictionary();
-        let string = "";
+        // empty dictionary before use
+        this.emptyArrays();
+        let string = ""; // S from the lecture
         let currentSymbol = "";
-        let compressedText = "";
+        let compressedText = ""; // z from the lecture
 
+        // iterates over all letters of the decompressed text
         for (let letter of decompressedText) {
             currentSymbol = letter;
-            let sz = [this.getStringFromDictionary(string), currentSymbol].join("");
+            // auxiliary variables for building a step
+            const _currentSymbol = currentSymbol;
+            const _string = string;
+
+            let sz = [this.getStringFromDictionary(string), currentSymbol].join(""); // joins S and z
             let nextFreeAddress = this.#dictionary.size + 256;
 
+            // check whether Sz is already in dictionary
             if (!this.#dictionary.has(sz) && string !== "") {
+                // add string or compound string (address) to compressed text
                 compressedText += this.getStringFromDictionary(string);
+                // add string or compound string (address) to dictionary
                 this.#dictionary.set([this.getStringFromDictionary(string), currentSymbol].join(""), nextFreeAddress);
                 string = currentSymbol;
             } else {
                 string = [this.getStringFromDictionary(string), currentSymbol].join("");
             }
+            this.#stepCollection.push(this.buildStep(_string, _currentSymbol, Array.from(this.#dictionary).pop()));
         }
+        // add string or compound string (address) to compressed text at the end of the process
         if (string !== "") compressedText += this.getStringFromDictionary(string);
-        return {compressedText: compressedText, dictionary: this.#dictionary};
+        return {compressedText: compressedText, dictionary: this.#dictionary, steps: this.#stepCollection};
     }
 
+    /**
+     * Decompresses a compressed string using the LZW algorithm
+     * @param compressedArray An array containing the single letters and addresses of the text to be decompressed.
+     * @returns {{decompressedText: string, dictionary: Map<any, any>, steps: []}} An object containing the resulting dictionary from the decompression and the decompressed text.
+     */
     decompressText(compressedArray) {
-        this.emptyDictionary();
+        // empty dictionary before use
+        this.emptyArrays();
         let string = "";
         let currentSymbol = "";
         let decompressedText = "";
 
         for (let i = 0; i < compressedArray.length; ++i) {
             currentSymbol = compressedArray[i];
+            // auxiliary variables to build a step
+            const _string = string;
+            const _currentSymbol = currentSymbol;
+
             let nextFreeAddress = this.#dictionary.size + 256;
-            console.log(`string = ${string}: ${this.getStringFromDictionary(string)}, current symbol: ${currentSymbol}`)
+            // while entry is an address
             while (currentSymbol.length > 1) {
-                // entry is address
-                console.log(compressedArray)
+                // extract address from whole string: <256> -> 256
                 const address = currentSymbol.substring(1, 4);
+                // remove address from array to replace with respective substring
                 compressedArray.splice(i, 1);
+                // get respective substring from dictionary
                 const partString = this.getStringByAddress(address);
                 const partArray = this.splitCompressedText(partString);
-                console.log(partArray)
+                // add substring as array to the array holding the compressed text
                 for (const entry of partArray.reverse()) {
                     compressedArray = insertAtIndex(compressedArray, i, entry);
                 }
+                // set current symbol to first symbol of added substring
                 currentSymbol = compressedArray[i];
             }
+            // add current symbol to the uncompressed text
             decompressedText += currentSymbol;
-            let sz = [string, currentSymbol].join("");
+            // update Sz
+            let sz = [this.getStringFromDictionary(string), currentSymbol].join("");
+            // check whether Sz is in dictionary and add it if necessary
             if (!this.#dictionary.has(sz) && string !== "") {
                 this.#dictionary.set([this.getStringFromDictionary(string), currentSymbol].join(""), nextFreeAddress);
                 string = "";
             }
-            string = [string, currentSymbol].join("");
+            // update S
+            string = [this.getStringFromDictionary(string), currentSymbol].join("");
+            this.#stepCollection.push(this.buildStep(_string, _currentSymbol, Array.from(this.#dictionary).pop()));
         }
-        console.log(this.#dictionary)
-        return {decompressedText: decompressedText, dictionary: this.#dictionary};
+        return {decompressedText: decompressedText, dictionary: this.#dictionary, steps: this.#stepCollection};
     }
 
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
 import {LwzConverter} from "./classes";
+import {TabulatorFull as Tabulator} from 'tabulator-tables';
+
+function buildTable(data) {
+    let table = new Tabulator('#table-test', {
+        data: data,
+        height: "100%",
+        layout:"fitDataStretch",
+        columns:[ //Define Table Columns
+            {title: "S with address", field: "S", headerSort: false},
+            {title: "z", field: "z", hozAlign: "left", headerSort: false},
+            {title: "New Entry", field: "newEntry", headerSort: false},
+        ],
+    });
+}
 
 function compress(textareaText) {
     let compressionResult = lzwConverter.compressText(textareaText);
     dictionaryTextarea.value = lzwConverter.stringifyDictionary();
     compressedTextarea.value = compressionResult.compressedText;
+    buildTable(compressionResult.steps);
 }
 
 function decompress(textareaText) {
@@ -11,22 +26,38 @@ function decompress(textareaText) {
     let compressionResult = lzwConverter.decompressText(decompression);
     dictionaryTextarea.value = lzwConverter.stringifyDictionary();
     decompressedTextarea.value = compressionResult.decompressedText;
+    buildTable(compressionResult.steps);
+}
 
+function clearAll() {
+    decompressedTextarea.value = "";
+    compressedTextarea.value = "";
+    dictionaryTextarea.value = "";
+    buildTable([]);
+}
+
+function clearDictionary() {
+    dictionaryTextarea.value = "";
+    buildTable([]);
 }
 
 let lzwConverter = new LwzConverter();
+
 const compressButton = document.getElementById("compress-button");
 const decompressButton = document.getElementById("decompress-button");
+const clearAllButton = document.getElementById("clear-all-button");
+const clearDictButton = document.getElementById("clear-dict-button");
+
 let compressedTextarea = document.getElementById("compressed-text");
 let decompressedTextarea = document.getElementById("decompressed-text");
 let dictionaryTextarea = document.getElementById("dictionary");
 
-compressButton.addEventListener("click", callback => {
-    const decompressedText = document.getElementById("decompressed-text").value;
-    compress(decompressedText);
-});
+buildTable([]);
+compressButton.addEventListener("click", callback => compress(decompressedTextarea.value));
+decompressButton.addEventListener("click", callback => decompress(compressedTextarea.value));
 
-decompressButton.addEventListener("click", callback => decompress(compressedTextarea.value))
+clearAllButton.addEventListener("click", clearAll);
+clearDictButton.addEventListener("click", clearDictionary);
 
 
 
